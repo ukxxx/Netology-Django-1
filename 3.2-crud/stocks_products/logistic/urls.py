ed26bdcf9aed0355a823026b3c90d576dff3b70a
uk <mail@mail.ru>
2023-11-02 13:12:34 +0300

    Add test veww by another way

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,10 +1,13 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 
 from logistic.views import ProductViewSet, StockViewSet, sample_view
 
 router = DefaultRouter()
 router.register("products", ProductViewSet)
 router.register("stocks", StockViewSet)
-router.register("test", sample_view)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("api/v1/test/", sample_view),
+    path("api/v1/", include(router.urls)),
+]
