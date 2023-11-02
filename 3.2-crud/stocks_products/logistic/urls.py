8904a1e3ea70b5260052841be17170d9e8dc1c89
uk <mail@mail.ru>
2023-11-02 13:20:23 +0300

    Fixed urls file mistake

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,5 +1,4 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import path, include
 
 from logistic.views import ProductViewSet, StockViewSet, sample_view
 
@@ -7,7 +6,4 @@ router = DefaultRouter()
 router.register("products", ProductViewSet)
 router.register("stocks", StockViewSet)
 
-urlpatterns = [
-    path("api/v1/test/", sample_view),
-    path("api/v1/", include(router.urls)),
-]
+urlpatterns = router.urls
