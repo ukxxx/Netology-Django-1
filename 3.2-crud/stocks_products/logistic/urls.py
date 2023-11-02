c52df1e6a8eca540ab55d229163bab52be69c96d
uk <mail@mail.ru>
2023-11-02 13:09:52 +0300

    Add test view

--- 3.2-crud/stocks_products/logistic/urls.py
+++ 3.2-crud/stocks_products/logistic/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from logistic.views import ProductViewSet, StockViewSet
+from logistic.views import ProductViewSet, StockViewSet, sample_view
 
 router = DefaultRouter()
 router.register("products", ProductViewSet)
 router.register("stocks", StockViewSet)
+router.register("test", sample_view)
 
 urlpatterns = router.urls
